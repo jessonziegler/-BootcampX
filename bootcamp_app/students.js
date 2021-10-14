87b49b74a34d3d15acf4bde2fe8529cf475d63c8
jessonziegler <jziegleraudio@gmail.com>
2021-10-14 05:11:30 +0000

    node-postgres

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -40,3 +40,24 @@ LIMIT ${process.argv[3] || 5};
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
 }).catch(err => console.error('query error', err.stack));
+
+const cohortName = '%' + process.argv[2] + '%';
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+const queryString = `
+SELECT students.id, students.name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name like $1
+LIMIT $2;
+`;
+
+pool.query(queryString, values)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
+    });
+  })
+  .catch(err => console.error('query error', err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -20,3 +20,23 @@ ORDER BY teacher;
     console.log(`${row.cohort}: ${row.teacher}`);
   })
 });
+const cohortName = '%' + process.argv[2] + '%';
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+const queryString = `
+SELECT teachers.id, teachers.name, cohorts.name as cohort
+FROM teachers
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name like $1
+LIMIT $2;
+`;
+
+pool.query(queryString, values)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
+    });
+  })
+  .catch(err => console.error('query error', err.stack));
